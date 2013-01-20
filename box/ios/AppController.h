aac745736b2a1b3473366c4213c8386b019642ce
al ra <alra@almatoMacBook-Pro.local>
2013-01-20 10:25:23 +0800

    Initial Commit

---
+++ box/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  boxAppController.h
+//  box
+//
+//  Created by ra al on 13-1-20.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
